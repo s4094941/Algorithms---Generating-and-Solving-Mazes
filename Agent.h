96a6068d71bd16e6fe80b7ca50b361b5ca70b695
Alex-D-4089452 <s4089452@student.rmit.edu.au>
2024-10-10 11:10:39 +1100

    Modified Agent.h/.cpp formatting

--- Agent.h
+++ Agent.h
@@ -3,37 +3,35 @@
 
 #include <iostream>
 #include <mcpp/mcpp.h>
+using mcpp::MinecraftConnection;
+using mcpp::Coordinate;
 
-#define MOVE_XPLUS mcpp::Coordinate(1,0,0)
-#define MOVE_XMINUS mcpp::Coordinate(-1,0,0)
-#define MOVE_ZPLUS mcpp::Coordinate(0,0,1)
-#define MOVE_ZMINUS mcpp::Coordinate(0,0,-1)
+#define MOVE_XPLUS Coordinate(1, 0, 0)
+#define MOVE_XMINUS Coordinate(-1, 0, 0)
+#define MOVE_ZPLUS Coordinate(0, 0, 1)
+#define MOVE_ZMINUS Coordinate(0, 0, -1)
 
-enum solveAlgorithm{
-        RIGHT_HAND_FOLLOW,
-        BREATH_FIRST_SEARCH,
+MinecraftConnection mc;
+
+enum solveAlgorithm {
+    RIGHT_HAND_FOLLOW,
+    BREATH_FIRST_SEARCH,
 };
 
-enum AgentOrientation{
+enum AgentOrientation {
     X_PLUS,
     Z_PLUS,
     X_MINUS,
     Z_MINUS
 };
 
-class Agent
-{
+class Agent {
+    public:
+        Agent(Coordinate startLoc);
+        ~Agent();
 
-public:
-    Agent(mcpp::Coordinate startLoc);
-    ~Agent();
-
-private:
-    /* data */
-    mcpp::MinecraftConnection mc;
+    private:
 
 };
 
-
-
-#endif //ASSIGN3_AGENT_H
+#endif // ASSIGN3_AGENT_H
