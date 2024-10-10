f73326a24d1b23b918149c6bd67d650e002393f8
Alex-D-4089452 <s4089452@student.rmit.edu.au>
2024-10-10 11:17:20 +1100

    Modified menuUtils.h formatting

--- menuUtils.h
+++ menuUtils.h
@@ -1,54 +1,46 @@
 #include <iostream>
+using std::cout;
+using std::endl;
 
-void printStartText(void){
-    std::cout << std::endl;
-    std::cout << "Welcome to MineCraft MazeRunner!" << std::endl;
-    std::cout << "--------------------------------" << std::endl;
+void printStartText() {
+    cout << endl << "Welcome to MineCraft MazeRunner!" << endl;
+    cout << "--------------------------------" << endl;
 }
 
-void printMainMenu(void){
-    std::cout << std::endl;
-    std::cout << "------------- MAIN MENU -------------" << std::endl;
-    std::cout << "1) Generate Maze" << std::endl;
-    std::cout << "2) Build Maze in MineCraft" << std::endl;
-    std::cout << "3) Solve Maze" << std::endl;
-    std::cout << "4) Show Team Information" << std::endl;
-    std::cout << "5) Exit" << std::endl;
-    std::cout << std::endl;
-    std::cout << "Enter Menu item to continue: " << std::endl;
+void printMainMenu() {
+    cout << endl << "------------- MAIN MENU -------------" << endl;
+    cout << "1) Generate Maze" << endl;
+    cout << "2) Build Maze in MineCraft" << endl;
+    cout << "3) Solve Maze" << endl;
+    cout << "4) Show Team Information" << endl;
+    cout << "5) Exit" << endl;
+    cout << endl << "Enter Menu item to continue: " << endl;
 }
 
-void printGenerateMazeMenu(void){
-    std::cout << std::endl;
-    std::cout << "------------- GENERATE MAZE -------------" << std::endl;
-    std::cout << "1) Read Maze from terminal" << std::endl;
-    std::cout << "2) Generate Random Maze" << std::endl;
-    std::cout << "3) Back" << std::endl;
-    std::cout << std::endl;
-    std::cout << "Enter Menu item to continue: " << std::endl;
+void printGenerateMazeMenu() {
+    cout << endl << "------------- GENERATE MAZE -------------" << endl;
+    cout << "1) Read Maze from terminal" << endl;
+    cout << "2) Generate Random Maze" << endl;
+    cout << "3) Back" << endl;
+    cout << endl << "Enter Menu item to continue: " << endl;
 }
 
-void printSolveMazeMenu(void){
-    std::cout << std::endl;
-    std::cout << "------------- SOLVE MAZE -------------" << std::endl;
-    std::cout << "1) Solve Manually" << std::endl;
-    std::cout << "2) Show Escape Route" << std::endl;
-    std::cout << "3) Back" << std::endl;
-    std::cout << std::endl;
-    std::cout << "Enter Menu item to continue: " << std::endl;
+void printSolveMazeMenu() {
+    cout << endl << "------------- SOLVE MAZE -------------" << endl;
+    cout << "1) Solve Manually" << endl;
+    cout << "2) Show Escape Route" << endl;
+    cout << "3) Back" << endl;
+    cout << endl << "Enter Menu item to continue: " << endl;
 }
 
-void printTeamInfo(void){
-    std::cout << std::endl;
-    std::cout << "Team members:" << std::endl;
-    std::cout << "\t [1] Alexander Davidson (s4089452@student.rmit.edu.au)" << std::endl;
-    std::cout << "\t [2] Caiden da Costa (s4904941@student.rmit.edu.au)" << std::endl;
-    std::cout << "\t [3] Jonas Inocencio (s3787701@student.rmit.edu.au)" << std::endl;
-    std::cout << std::endl;
+void printTeamInfo() {
+    cout << endl << "Team members:" << endl;
+    cout << "\t [1] Alexander Davidson (s4089452@student.rmit.edu.au)" << endl;
+    cout << "\t [2] Caiden da Costa (s4904941@student.rmit.edu.au)" << endl;
+    cout << "\t [3] Jonas Inocencio (s3787701@student.rmit.edu.au)" << endl;
+    cout << endl;
 }
 
-void printExitMassage(void){
-    std::cout << std::endl;
-    std::cout << "The End!" << std::endl;
-    std::cout << std::endl;
+void printExitMassage() {
+    cout << endl << "The End!" << endl << endl;
 }
