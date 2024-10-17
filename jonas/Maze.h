e3ec2ba6ed0982e6233d91a3d2a49afdbd8506a8
Jonas Inocencio <s3787701@student.rmit.edu.au>
2024-10-18 10:01:50 +1100

    Formatting

--- jonas/Maze.h
+++ jonas/Maze.h
@@ -1,12 +1,3 @@
-// MazeNode*** maze = new MazeNode**[row];
-//     for (int i = 0; i < row; ++i) {
-//         maze[i] = new MazeNode*[col];
-//         for (int j = 0; j < col; ++j) {
-//             maze[i][j] = new MazeNode(i, j);
-//         }
-//     }
-
-
 #ifndef MAZE_H
 #define MAZE_H
 #include "MazeNode.h"
@@ -25,6 +16,6 @@ class Maze {
 #endif // MAZE_H
 
 
-// Check if user-generated maze if valid:
+// TODO: Check if user-generated maze if valid:
     // check edge of maze. If open areas != 1, invalid
     // if not perfect maze (i.e. existence of a loop), invalid
--- jonas/MazeNode.cpp
+++ jonas/MazeNode.cpp
@@ -2,94 +2,83 @@
 #include <cstdlib>
 #include <iostream>
 
-class MazeNode {
-    private:
-        int row, col;
-        int dirCount = 4;
-        bool up = false, down = false, left = false, right = false;
-        bool isWall = true, explored = false;
-        MazeNode* prevNode = nullptr;
-    // TODO: Change bools to bitfield 000000
+// Constructor
+MazeNode::MazeNode(int r, int c) : row(r), col(c), dirCount(4), up(false), down(false), left(false), right(false),
+                                   isWall(true), explored(false), prevNode(nullptr) {}
 
-    public:
-    // Constructor
-        MazeNode (int r, int c) : row(r), col(c) {}
+// Basic mutators
+void MazeNode::setWall (bool status) {
+    isWall = status;
+}
+void MazeNode::setExplored(bool status) {
+    explored = status; isWall = false;
+}
+void MazeNode::setPrevNode (MazeNode* prev) {
+    prevNode = prev;
+}
+// Set directions as checked, and decrement number of valid directions
+void MazeNode::markUp()    {
+    up    = true; --dirCount;
+}
+void MazeNode::markDown()  {
+    down  = true; --dirCount;
+}
+void MazeNode::markLeft()  {
+    left  = true; --dirCount;
+}
+void MazeNode::markRight() {
+    right = true; --dirCount;
+}
 
-    // Basic mutators
-        void setWall (bool status) {
-            isWall = status;
-        }
-        void setExplored(bool status) {
-            explored = status; isWall = false;
-        }
-        void setPrevNode (MazeNode* prev) {
-            prevNode = prev;
-        }
-    // Set directions as checked, and decrement number of valid directions
-        void markUp()    {
-            up    = true; --dirCount;
-        }
-        void markDown()  {
-            down  = true; --dirCount;
-        }
-        void markLeft()  {
-            left  = true; --dirCount;
-        }
-        void markRight() {
-            right = true; --dirCount;
-        }
-    
-    // Basic accessors
-        int getRow() {
-            return row;
-        }
-        int getCol() {
-            return col;
-        }
-        int getDirCount() {
-            return dirCount;
-        }
-        bool getStatus() {
-            return explored;
-        }
-        MazeNode* getPrevNode() {
-            if (prevNode != nullptr) { return prevNode; }
-            return NULL;
-        }
+// Basic accessors
+int MazeNode::getRow() {
+    return row;
+}
+int MazeNode::getCol() {
+    return col;
+}
+int MazeNode::getDirCount() {
+    return dirCount;
+}
+bool MazeNode::getStatus() {
+    return explored;
+}
+MazeNode* MazeNode::getPrevNode() {
+    if (prevNode != nullptr) { return prevNode; }
+    return NULL;
+}
 
-    // Return a random direction from available directions
-    // UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3;
-        int getRandomDirection() {
-            if (dirCount <= 0) { return -1; }
+// Return a random direction from available directions
+// UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3;
+int MazeNode::getRandomDirection() {
+    if (dirCount <= 0) { return -1; }
 
-            int count = 0;
-            int dirList[4];
-            if (!up) {
-                dirList[count++] = 0;
-            }
-            if (!down) {
-                dirList[count++] = 1;
-            }
-            if (!left) {
-                dirList[count++] = 2;
-            }
-            if (!right) {
-                dirList[count++] = 3;
-            }
+    int count = 0;
+    int dirList[4];
+    if (!up) {
+        dirList[count++] = 0;
+    }
+    if (!down) {
+        dirList[count++] = 1;
+    }
+    if (!left) {
+        dirList[count++] = 2;
+    }
+    if (!right) {
+        dirList[count++] = 3;
+    }
 
-            int direction = dirList[rand() % dirCount];
+    int direction = dirList[rand() % dirCount];
 
-            return direction;
-        }
+    return direction;
+}
 
-        void printNode() {
-            if (isWall) {
-                std::cout << 'x';
-            } else if (explored) {
-                std::cout << '.';
-            } else {
-                std::cout << dirCount;
-            }
-        }
-
-};
+// Print Node
+void MazeNode::printNode() {
+    if (isWall) {
+        std::cout << 'x';
+    } else if (explored) {
+        std::cout << '.';
+    } else {
+        std::cout << dirCount;
+    }
