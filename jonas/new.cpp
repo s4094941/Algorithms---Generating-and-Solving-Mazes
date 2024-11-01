8388f1138bcdad5a22990f5cb6921d462bebb298
Jonas Inocencio <s3787701@student.rmit.edu.au>
2024-11-01 21:45:00 +1100

    Created a file to document all new changes for E3

---
+++ jonas/new.cpp
@@ -0,0 +1,132 @@
+// TO DO LIST:
+    // for get start point, mark direction to check as invalid based on position (maybe unnecessary)
+
+
+// Maze.h ================================================
+private:
+    void resetAll();
+    void checkEdge(MazeNode*);
+    MazeNode* findStartPoint();
+
+public:
+    void checkValidity();
+
+// Maze.cpp
+void Maze::resetAll() {
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            maze[i][j]->resetNode();
+            checkEdge(maze[i][j]);
+        }
+    }
+}
+
+void Maze::checkEdge(MazeNode* node) {
+    if (node->getRow() == 0 || node->getRow() == 1) {
+        node->markUp();
+    }
+    if (node->getCol() == 0 || node->getCol() == 1) {
+        node->markLeft();
+    }
+    if (node->getRow() == row - 1 || node->getRow() == row - 2) {
+        node->markDown();
+    }
+    if (node->getCol() == col - 1 || node->getCol() == col - 2) {
+        node->markRight();
+    }
+}
+
+MazeNode* Maze::findStartPoint() {
+    MazeNode* startPoint = nullptr;
+
+    // CHECK TOP ROW
+    for (int i = 1; i < row - 1; ++i) {
+        if (maze[0][i]->getWall() == false) {
+            if (startPoint == nullptr) {
+                startPoint = maze[0][i];
+            } else {
+                std::cout << "Multiple entry points detected. Node at "
+                << "[0][" << i << "]"
+                << " converted to a wall." << std::endl;
+                maze[0][i]->setWall(true);
+            }
+        }
+    }
+
+    // CHECK BOTTOM ROW
+    for (int i = 1; i < row - 1; ++i) {
+        if (maze[row - 1][i]->getWall() == false) {
+            if (startPoint == nullptr) {
+                startPoint = maze[row - 1][i];
+            } else {
+                std::cout << "Multiple entry points detected. Node at "
+                << "[" << row - 1 << "][" << i << "]"
+                << " converted to a wall." << std::endl;
+                maze[row - 1][i]->setWall(true);
+            }
+        }
+    }
+
+    // CHECK LEFT COLUMN
+    for (int i = 1; i < col - 1; ++i) {
+        if (maze[i][0]->getWall() == false) {
+            if (startPoint == nullptr) {
+                startPoint = maze[i][0];
+            } else {
+                std::cout << "Multiple entry points detected. Node at "
+                << "[" << i << "][0]"
+                << " converted to a wall." << std::endl;
+                maze[i][0]->setWall(true);
+            }
+        }
+    }
+
+    // CHECK RIGHT COLUMN
+    for (int i = 0; i < col - 1; ++i) {
+        if (maze[i][col - 1]->getWall() == false) {
+            if (startPoint == nullptr) {
+                startPoint = maze[i][col - 1];
+            } else {
+                std::cout << "Multiple entry points detected. Node at "
+                << "[" << i << "][" << col - 1 << "]"
+                << " converted to a wall." << std::endl;
+                maze[i][col - 1]->setWall(true);
+            }
+        }
+    }
+
+    return startPoint;
+}
+
+
+void Maze::checkValidity() {
+    MazeNode* startNode = nullptr;
+    MazeNode* curr = nullptr;
+    
+    resetAll();
+    startNode = findStartPoint();
+    curr = startNode;
+
+    while (curr != nullptr) {
+
+    }
+
+}
+
+// mazeNode.h ==============================================
+private:
+
+public:
+    void resetNode();
+
+// MazeNode.cpp
+void MazeNode::resetNode() {
+    up = false;
+    down = false;
+    left = false;
+    right = false;
+    explored = false;
+    prevNode = nullptr;
+    dirCount = 4;
+    explored = false;
+}
